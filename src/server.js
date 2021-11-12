be0cb6a36ce78e55186bf2883da6dbd7ed1454a0
fabio <fabioluizfaria@gmail.com>
2021-11-12 16:15:17 -0400

    Sequelize

--- src/server.js
+++ src/server.js
@@ -1,5 +1,25 @@
 import app from './app';
 
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 const PORTA = process.env.PORT || 8080
 
 app.listen(PORTA, function() {
