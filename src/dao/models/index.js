1a9973af33956e935dbe543d46f93275d09b45e7
Fabio Luiz Faria da Silva <65410834+fabioluizfaria@users.noreply.github.com>
2024-09-20 16:41:07 -0400

    Update index.js

--- src/dao/models/index.js
+++ src/dao/models/index.js
@@ -8,17 +8,17 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-/* let sequelize;
+let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-} */
+}
 
 /**
  * CONEXÃO COM HEROKU
  */
- let sequelize = new Sequelize(process.env.DATABASE_URL, { 
+ /*let sequelize = new Sequelize(process.env.DATABASE_URL, { 
   dialectOptions: {
     ssl: {
       require: true,
@@ -26,7 +26,7 @@ if (config.use_env_variable) {
     }
   }
 }
-);
+);*/
 
 fs
   .readdirSync(__dirname)
